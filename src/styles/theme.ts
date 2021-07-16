f81002df7195a09d14a2a825ebce3fa36d6598dd
thiago.oliveira <thiago.oliveira@intera.org.br>
2021-07-16 13:55:52 -0400

    add many adjusts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -13,6 +13,17 @@ export const theme = extendTheme({
            "200": "#B3B5C6",
            "100": "#D1D2DC",
            "50": "#EEEEF2"
+        },
+        green: {
+            100: "#E5FCF1",
+            200: "#27EF96",
+            300: "#10DE82",
+            400: "#0EBE6F",
+            500: "#0CA25F",
+            600: "#0A864F",
+            700: "#086F42",
+            800: "#075C37",
+            900: "#064C2E"
         }
     },
     fonts: {
@@ -22,8 +33,8 @@ export const theme = extendTheme({
     styles: {
         global: {
             body: {
-                bg: 'gray.900',
-                color: 'gray.50',
+                bg: 'white',
+                color: 'gray.900',
             }
         }
     }
